d45c19c22b703b3132e6ca7bdfda33a7eea51be7
shahLLL <shahlll@yahoo.com>
2026-08-19 00:52:05 -0700

    Add internal data types to engine and fix issues, details below.
    Issues Fixed:
    - Public/Private Accessor's properly define in Engine Class
    - Function/Method signature fixed for "getBids" and "getAsks" methods

--- headers/engine.hpp
+++ headers/engine.hpp
@@ -2,6 +2,7 @@
 
 #include "types.hpp"
 #include <optional>
+#include <list>
 
 // Aliases
 using Events = std::vector<Event>;
@@ -9,12 +10,30 @@ using Levels = std::vector<Level>;
 using TOB = std::optional<Level>;
 
 class Engine final {
-    Engine() = default;
-    Events submitOrder(const Order& order);
-    Events cancelOrder(UnsignedLong orderID);
-    TOB getBestBid() const noexcept;
-    TOB getBestAsk() const noexcept;
-    Levels getBids() const;
-    Levels getAsks() const;
-    ~Engine() = default;
+    // Internal Types, private by default.
+    struct LevelOrder {
+        UnsignedLong orderID;
+        UnsignedLong orderPrice;
+        UnsignedLong orderQuantity;
+        TimeStamp orderTimeStamp;
+    };
+    struct PriceLevel {
+        std::list<LevelOrder> level;
+        UnsignedLong totalQuantity;
+    };
+    struct Locator {
+        Side orderSide;
+        SignedLong orderPrice;
+        std::list<LevelOrder>::iterator levelItr;
+    };
+
+    public:
+        Engine() = default;
+        Events submitOrder(const Order& order);
+        Events cancelOrder(UnsignedLong orderID);
+        TOB getBestBid() const noexcept;
+        TOB getBestAsk() const noexcept;
+        Levels getBids(UnsignedLong n) const;
+        Levels getAsks(UnsignedLong n) const;
+        ~Engine() = default;
 };

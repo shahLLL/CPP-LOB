9afde73b7aae8382ce8dbc43531f6d0665268e23
shahLLL <shahlll@yahoo.com>
2026-08-27 09:38:12 -0700

    Add test cases for getMidPrice method
    ISSUE: https://github.com/shahLLL/CPP-LOB/issues/17

---
+++ tests/mid_price.cpp
@@ -0,0 +1,66 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../headers/lob.hpp"
+
+TEST_CASE("BID ASK SPREAD TESTCASE #1", "[mid_price]") {
+    // Check Mid Price for empty order book.
+    LOB limitOrderBook = LOB();
+    REQUIRE(limitOrderBook.getMidPrice() == 0.0);
+}
+
+TEST_CASE("BID ASK SPREAD TESTCASE #2", "[mid_price]") {
+    // Check Mid Price for bids and no asks.
+    LOB limitOrderBook = LOB();
+    Price testPrice = 34.9;
+    limitOrderBook.submitOrder(Order {
+        1,
+        testPrice,
+        1,
+        Side::BUY,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+
+    });
+    REQUIRE(limitOrderBook.getMidPrice() == testPrice);
+}
+
+TEST_CASE("BID ASK SPREAD TESTCASE #3", "[mid_price]") {
+    // Check Mid Price for asks and no bids.
+    LOB limitOrderBook = LOB();
+    Price testPrice = 34.9;
+    limitOrderBook.submitOrder(Order {
+        1,
+        testPrice,
+        1,
+        Side::SELL,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+
+    });
+    REQUIRE(limitOrderBook.getMidPrice() == testPrice);
+}
+
+TEST_CASE("BID ASK SPREAD TESTCASE #4", "[mid_price]") {
+    // Check Mid Price for asks and bids.
+    LOB limitOrderBook = LOB();
+    Price testPriceBid = 34.0;
+    Price testPriceAsk = 54.0;
+    limitOrderBook.submitOrder(Order {
+        1,
+        testPriceAsk,
+        1,
+        Side::SELL,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+
+    });
+    limitOrderBook.submitOrder(Order {
+        2,
+        testPriceBid,
+        1,
+        Side::BUY,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+
+    });
+    REQUIRE(limitOrderBook.getMidPrice() == 44.0);
+}

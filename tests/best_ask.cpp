eafdaf415e62d64d6c61fd765d5c4b25f54122bb
shahLLL <shahlll@yahoo.com>
2026-08-26 23:52:03 -0700

    Add test cases for getBestAsk method
    ISSUE: https://github.com/shahLLL/CPP-LOB/issues/13

--- tests/best_ask.cpp
+++ tests/best_ask.cpp
@@ -70,3 +70,59 @@ TEST_CASE("BEST ASK TEST CASE #1", "[get_best_ask]") {
     REQUIRE(levelOrderBook.getBestAsk().value().price == testPrice4);
     REQUIRE(levelOrderBook.getBestAsk().value().quantity == testQuantity4);
 }
+
+TEST_CASE("BEST ASK TEST CASE #2", "[get_best_ask]") {
+    // Best Ask not effect by Buy Side orders
+    LOB levelOrderBook = LOB();
+
+    Price testPrice1 = 124.51;
+    Quantity testQuantity1 = 40;
+    Price testPrice2 = testPrice1 * 2.0;
+    Quantity testQuantity2 = testQuantity1 / 2;
+    Price testPrice3 = testPrice1 / 2.0;
+    Quantity testQuantity3 = testQuantity1 + 2;
+    Price testPrice4 = testPrice1 / 1.5;
+    Quantity testQuantity4 = testQuantity1 + 2;
+    
+    REQUIRE(!levelOrderBook.getBestAsk().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        1,
+        testPrice1,
+        testQuantity1,
+        Side::BUY,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestAsk().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        2,
+        testPrice2,
+        testQuantity2,
+        Side::BUY,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestAsk().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        3,
+        testPrice3,
+        testQuantity3,
+        Side::BUY,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestAsk().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        4,
+        testPrice4,
+        testQuantity4,
+        Side::BUY,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestAsk().has_value());
+}

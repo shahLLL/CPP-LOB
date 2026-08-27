58a7db27d609f776e967354d067ddc882d39af90
shahLLL <shahlll@yahoo.com>
2026-08-26 23:55:36 -0700

    Add test cases for getBestBid method
    ISSUE: https://github.com/shahLLL/CPP-LOB/issues/12

--- tests/best_bid.cpp
+++ tests/best_bid.cpp
@@ -70,3 +70,59 @@ TEST_CASE("BEST BID TEST CASE #1", "[get_best_bid]") {
     REQUIRE(levelOrderBook.getBestBid().value().price == testPrice4);
     REQUIRE(levelOrderBook.getBestBid().value().quantity == testQuantity4);
 }
+
+TEST_CASE("BEST BID TEST CASE #2", "[get_best_bid]") {
+    // Best Bid not effect by Sell Side orders
+    LOB levelOrderBook = LOB();
+
+    Price testPrice1 = 12.5;
+    Quantity testQuantity1 = 5;
+    Price testPrice2 = testPrice1 / 2.0;
+    Quantity testQuantity2 = testQuantity1 * 2;
+    Price testPrice3 = testPrice1 * 3.0;
+    Quantity testQuantity3 = testQuantity1 - 2;
+    Price testPrice4 = testPrice1 * 2.0;
+    Quantity testQuantity4 = testQuantity1 - 2;
+    
+    REQUIRE(!levelOrderBook.getBestBid().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        1,
+        testPrice1,
+        testQuantity1,
+        Side::SELL,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestBid().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        2,
+        testPrice2,
+        testQuantity2,
+        Side::SELL,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestBid().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        3,
+        testPrice3,
+        testQuantity3,
+        Side::SELL,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestBid().has_value());
+
+    levelOrderBook.submitOrder(Order {
+        4,
+        testPrice4,
+        testQuantity4,
+        Side::SELL,
+        OrderType::LIMIT,
+        TimeInForce::GTC
+    });
+    REQUIRE(!levelOrderBook.getBestBid().has_value());
+}
